76aff8d17d4a2cc9644b7a65f7cfba078ceae006
Vina Tan <88601826+vinatsx@users.noreply.github.com>
2022-10-06 01:13:41 +0800

    Create db.py

---
+++ db.py
@@ -0,0 +1,11 @@
+# Simulated db
+db = [{
+    "name": "Chun Yu",
+    "scores": [1, 3, 3, 4, 5]
+}, {
+    "name": "Marcus",
+    "scores": [5, 4, 3, 2, 1]
+}, {
+    "name": "Austin",
+    "scores": [9, 29, 34, 1]
+}]
