b6908df9cda702f0aadbd767b62d1e51bf8036a1
vitorio-p <64684609+vitorio-p@users.noreply.github.com>
2022-10-24 17:59:37 +0800

    moved the tasks into the appropriate files

--- Profiles/ProfilesAPI.py
+++ Profiles/ProfilesAPI.py
@@ -4,3 +4,10 @@ from db import db
 sys.path.append("../")
 
 profiles_api = Blueprint("profiles", __name__)
+
+@app.route("/profiles/<id>", methods = ["GET", "POST", "DELETE"])
+def user(user_id):
+
+    # GET /{id} to retrieve the name and all scores of a profile 
+    if request.method == "GET":
+        return db[int(user_id)]
--- main.py
+++ main.py
@@ -15,33 +15,5 @@ app.register_blueprint(auth_api, url_prefix="/auth")
 def hello_world():
     return "welcome to vito's scrappy website"
 
-
-@app.route("/profiles/<user_id>", methods = ["GET"])
-def user(user_id):
-    # GET /{id} to retrieve the name and all scores of a profile 
-    return db[int(user_id)]
-
-'''
-
-@app.route("/profiles/<user_id>", methods = ["GET", "POST", "DELETE"])
-def user(user_id):
-
-    # GET /{id} to retrieve the name and all scores of a profile 
-    if request.method == "GET":
-        return db[int(user_id)]
-    
-    # POST /profiles to create a new profile (name only) 
-    if request.method == "POST":
-        name = flask.request.values.get('name')
-
-    # DELETE /{id} to delete a profile 
-    if request.method == "DELETE":
-        return "2"
-
-    # catch-all
-    else:
-        return "go back"
-'''
-
 if __name__ == "__main__":
     app.run("0.0.0.0", port=8080)
