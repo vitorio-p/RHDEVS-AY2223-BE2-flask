264dab61705a92dfb3d2a3fa03f1a7520096112c
vitorio-p <64684609+vitorio-p@users.noreply.github.com>
2022-10-24 17:27:57 +0800

    completed task 1 and 2

--- main.py
+++ main.py
@@ -3,10 +3,45 @@ from Profiles.ProfilesAPI import profiles_api
 from flask import Flask
 from db import db
 
-
-# Write your flask code here
-
 app = Flask(__name__)
 
 app.register_blueprint(profiles_api, url_prefix="/profiles")
 app.register_blueprint(auth_api, url_prefix="/auth")
+
+# Write your flask code here
+
+# GET / homepage that returns a welcome message 
+@app.route("/", methods = ["GET"])
+def hello_world():
+    return "welcome to vito's scrappy website"
+
+
+@app.route("/profiles/<user_id>", methods = ["GET"])
+def user(user_id):
+    # GET /{id} to retrieve the name and all scores of a profile 
+    return db[int(user_id)]
+
+'''
+
+@app.route("/profiles/<user_id>", methods = ["GET", "POST", "DELETE"])
+def user(user_id):
+
+    # GET /{id} to retrieve the name and all scores of a profile 
+    if request.method == "GET":
+        return db[int(user_id)]
+    
+    # POST /profiles to create a new profile (name only) 
+    if request.method == "POST":
+        name = flask.request.values.get('name')
+
+    # DELETE /{id} to delete a profile 
+    if request.method == "DELETE":
+        return "2"
+
+    # catch-all
+    else:
+        return "go back"
+'''
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=8080)
